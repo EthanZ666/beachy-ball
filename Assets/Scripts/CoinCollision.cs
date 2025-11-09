877012cbfb49eb2f64945337de655f4acf053f8b
EthanZ666 <zhao1fanethan@gmail.com>
2025-11-08 22:42:52 -0500

    CoinCollision

---
+++ Assets/Scripts/CoinCollision.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting; // needed for List
+
+public class CoinCollision : MonoBehaviour
+{
+    public MoneyManager moneyManager;
+
+
+    public Coin coin;
+
+
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Coin>() != null)
+        // Set to player after
+        {
+            moneyManager.AddMoney(coin.value);
+            Destroy(coin);
+        }
+    }
+
+}
